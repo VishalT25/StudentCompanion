b3e06f413eaf90670f26e2c824a387a37ce9e40d
Vishal Thamaraimanalan <vthamaraimanalan@gmail.com>
2025-05-29 16:00:49 -0400

    Initial commit for checkpoints

---
+++ StudentCompanionTests/StudentCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudentCompanionTests.swift
+//  StudentCompanionTests
+//
+//  Created by Vishal Thamaraimanalan on 2025-05-16.
+//
+
+import Testing
+@testable import StudentCompanion
+
+struct StudentCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
